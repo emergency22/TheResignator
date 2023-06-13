b8dd96c2e53e42793b64cc1e4f98672b35388608
emergency22 <alex.d.murillo@protonmail.com>
2023-06-13 08:38:39 -0500

    Add organization and position

--- src/main/java/service/request/EmailGenerationRequest.java
+++ src/main/java/service/request/EmailGenerationRequest.java
@@ -6,6 +6,8 @@ import java.util.Date;
 public class EmailGenerationRequest {
     private final String recipientAddress;
     private final String recipientName;
+    private final String position;
+    private final String organization;
     private final String signature;
     private final String senderAddress;
     private final String senderPhone;
@@ -14,9 +16,11 @@ public class EmailGenerationRequest {
 
     private final SimpleDateFormat lastDay;
 
-    public EmailGenerationRequest(String recipientAddress, String recipientName, String signature, String senderAddress, String senderPhone, SimpleDateFormat executionDate, SimpleDateFormat lastDay) {
+    public EmailGenerationRequest(String recipientAddress, String recipientName, String position, String organization, String signature, String senderAddress, String senderPhone, SimpleDateFormat executionDate, SimpleDateFormat lastDay) {
         this.recipientAddress = recipientAddress;
         this.recipientName = recipientName;
+        this.position = position;
+        this.organization = organization;
         this.signature = signature;
         this.senderAddress = senderAddress;
         this.senderPhone = senderPhone;
@@ -30,6 +34,14 @@ public class EmailGenerationRequest {
 
     public String getRecipientName() {
         return recipientName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getOrganization() {
+        return organization;
     }
 
     public String getSignature() {
