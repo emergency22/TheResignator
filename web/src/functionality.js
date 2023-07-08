57112d21387c0eb5bf1e8526c5d2373e14d0b12b
emergency22 <alex.d.murillo@protonmail.com>
2023-07-08 16:21:20 -0500

    Start to add Javascript functionality

---
+++ web/src/functionality.js
@@ -0,0 +1,36 @@
+function confirmQuit(event) {
+    event.preventDefault(); // Prevents the default form submission
+
+    const quitJob = "Quit my job";
+    const cancel = "Cancel";
+
+    const confirmation = confirm("Are you sure you want to quit your job? There are no takesies backsies.");
+
+    if (confirmation) {
+        // User clicked "OK" or "Yes"
+        // Perform the action to quit the job
+        // Replace this with your own logic or redirect to a new page
+        console.log("Quitting job...");
+    } else {
+        // User clicked "Cancel" or "No"
+        // Do nothing or perform any other desired action
+        console.log("Job quitting canceled.");
+    }
+}
+
+
+
+// addHoseFormSubmitter(fireDept, apparatusTypeAndNumber) {
+//     var addHoseFormToClient = document.getElementById('addHoseForm');
+//     addHoseFormToClient.addEventListener('submit', async (event) => {
+//         event.preventDefault()  //prevents auto-submit
+//
+//         var inputName = document.getElementById('name').value;
+//         var inputColor = document.getElementById('colorSelector').value;
+//         var inputLength = document.getElementById('lengthSelector').value;
+//         var inputDiameter = document.getElementById('diameterSelector').value;
+//         var inputGallons = document.getElementById('gpmSelector').value;
+//
+//         await this.client.addHose(fireDept, apparatusTypeAndNumber, inputName, inputColor, inputLength, inputDiameter, inputGallons);
+//         await this.displayHoses(fireDept, apparatusTypeAndNumber);
+//     });
