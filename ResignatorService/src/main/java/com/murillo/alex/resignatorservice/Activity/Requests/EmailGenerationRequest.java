e8956812fc9ff2c36f2cfe54d016cdb592f02aca
emergency22 <alex.d.murillo@protonmail.com>
2023-07-15 13:49:34 -0500

    Built out EmailGenerationRequest class

--- ResignatorService/src/main/java/com/murillo/alex/resignatorservice/Activity/Requests/EmailGenerationRequest.java
+++ ResignatorService/src/main/java/com/murillo/alex/resignatorservice/Activity/Requests/EmailGenerationRequest.java
@@ -1,42 +1,44 @@
 package com.murillo.alex.resignatorservice.Activity.Requests;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
 import java.text.SimpleDateFormat;
 
+@JsonDeserialize(builder = EmailGenerationRequest.Builder.class)
 public class EmailGenerationRequest {
-    private final String recipientAddress;
-    private final String recipientFirstName;
+    private final String senderEmail;
+    private final String firstName;
+    private final String lastName;
     private final String position;
     private final String organization;
-    private final String senderFirstName;
-    private final String senderLastName;
-    private final String senderAddress;
-    private final String senderPhone;
-
+    private final String recipientFirstName;
+    private final String recipientEmail;
     private final SimpleDateFormat executionDate;
-
     private final SimpleDateFormat lastDay;
 
-    public EmailGenerationRequest(String recipientAddress, String recipientFirstName, String senderLastName, String position, String organization, String senderFirstName, String senderAddress, String senderPhone, SimpleDateFormat executionDate, SimpleDateFormat lastDay) {
-        this.recipientAddress = recipientAddress;
-        this.recipientFirstName = recipientFirstName;
+    public EmailGenerationRequest(String senderEmail, String firstName, String lastName, String position, String organization, String recipientFirstName, String recipientEmail, SimpleDateFormat executionDate, SimpleDateFormat lastDay) {
+        this.senderEmail = senderEmail;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.position = position;
         this.organization = organization;
-        this.senderFirstName = senderFirstName;
-        this.senderLastName = senderLastName;
-        this.senderAddress = senderAddress;
-        this.senderPhone = senderPhone;
+        this.recipientFirstName = recipientFirstName;
+        this.recipientEmail = recipientEmail;
         this.executionDate = executionDate;
         this.lastDay = lastDay;
     }
 
-
-
-    public String getRecipientAddress() {
-        return recipientAddress;
+    public String getSenderEmail() {
+        return senderEmail;
     }
 
-    public String getRecipientFirstName() {
-        return recipientFirstName;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 
     public String getPosition() {
@@ -47,16 +49,12 @@ public class EmailGenerationRequest {
         return organization;
     }
 
-    public String getSenderFirstName() {
-        return senderFirstName;
+    public String getRecipientFirstName() {
+        return recipientFirstName;
     }
 
-    public String getSenderAddress() {
-        return senderAddress;
-    }
-
-    public String getSenderPhone() {
-        return senderPhone;
+    public String getRecipientEmail() {
+        return recipientEmail;
     }
 
     public SimpleDateFormat getExecutionDate() {
@@ -66,4 +64,87 @@ public class EmailGenerationRequest {
     public SimpleDateFormat getLastDay() {
         return lastDay;
     }
+
+    @Override
+    public String toString() {
+        return "EmailGenerationRequest{" +
+                "senderEmail='" + senderEmail + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", position='" + position + '\'' +
+                ", organization='" + organization + '\'' +
+                ", recipientFirstName='" + recipientFirstName + '\'' +
+                ", recipientEmail='" + recipientEmail + '\'' +
+                ", executionDate=" + executionDate +
+                ", lastDay=" + lastDay +
+                '}';
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @JsonPOJOBuilder
+    public static class Builder {
+        private String senderEmail;
+        private String firstName;
+        private String lastName;
+        private String position;
+        private String organization;
+        private String recipientFirstName;
+        private String recipientEmail;
+        private SimpleDateFormat executionDate;
+        private SimpleDateFormat lastDay;
+
+        public Builder withSenderEmail(String senderEmail) {
+            this.senderEmail = senderEmail;
+            return this;
+        }
+
+        public Builder withFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder withLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder withPosition(String position) {
+            this.position = position;
+            return this;
+        }
+
+        public Builder withOrganization(String organization) {
+            this.organization = organization;
+            return this;
+        }
+
+        public Builder withRecipientFirstName(String recipientFirstName) {
+            this.recipientFirstName = recipientFirstName;
+            return this;
+        }
+
+        public Builder withRecipientEmail(String recipientEmail) {
+            this.recipientEmail = recipientEmail;
+            return this;
+        }
+
+        public Builder withExecutionDate(SimpleDateFormat executionDate) {
+            this.executionDate = executionDate;
+            return this;
+        }
+
+        public Builder withLastDay(SimpleDateFormat lastDay) {
+            this.lastDay = lastDay;
+            return this;
+        }
+
+        public EmailGenerationRequest build() {
+            return new EmailGenerationRequest(senderEmail, firstName, lastName, position, organization, recipientFirstName, recipientEmail, executionDate, lastDay);
+        }
+
+    }
+
 }
